128bbea181f23b481c43dde32aec96af887a0c49
FabianMC17 <fabian.maquen@usil.pe>
2023-11-16 00:22:02 -0500

    Fabian

--- PruebaFinal/src/GUI/Hola.java
+++ PruebaFinal/src/GUI/Hola.java
@@ -8,12 +8,12 @@ package GUI;
  *
  * @author Fabian Maquen
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Hola extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form Hola
      */
-    public NewJFrame() {
+    public Hola() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Hola.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Hola.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Hola.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Hola.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Hola().setVisible(true);
             }
         });
     }
